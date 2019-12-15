92133e29c9fcbbb26920d5916cbc5a63b888a718
bkcastille <50145813+bkcastille@users.noreply.github.com>
2019-12-15 10:52:05 -0500

    Add files via upload
    Alright guys, the meat of what I did was in

--- Account.java
+++ Account.java
@@ -1,61 +1,59 @@
 import java.io.Serializable;
-
 //class to set account info
 public class Account implements Serializable {
-    protected Customer customer;
-    protected int accountNumber;
-    protected float balance;
-    protected int transaction;
-    protected float monthlyEnd;
-    protected String accountType;
+ protected Customer customer;
+ protected int accountNumber;
+ protected float balance;
+ protected int transaction;
+ protected float monthlyEnd;
+ protected String accountType;
+ //getter so account info can be referenced elsewhere
+ public Account(Customer customer, int accountNumber, float balance, int
+	 transaction, String accountType) {
+ this.customer = customer;
+ this.accountNumber = accountNumber;
+ this.balance = balance;
+ this.transaction = transaction;
+ this.accountType = accountType;
+ }
+ //setters for attributes
+ public void setAccount ( int accountNumber ) {
+ this.accountNumber = accountNumber;
+ }
+ public void setBalance ( float balance ) {
+ this.balance = balance;
+ }
+ public void setTransaction ( int transaction ) {
+ this.transaction = transaction;
+ }
+ public void setMonthlyEnd ( float monthlyEnd ) {
+ this.monthlyEnd = monthlyEnd;
+ }
+ public void setCustomer ( Customer customer ) {
+ this.customer = customer;
+ }
+ public void setAccountType ( String accountType) {
+ this.accountType = accountType;
+ }
 
-    //getter so account info can be referenced elsewhere
-    public Account(Customer customer, int accountNumber, float balance, int transaction, String accountType) {
-        this.customer = customer;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-        this.transaction = transaction;
-        this.accountType = accountType;
-    }
+ //getters for attributes
+ public int getAccountNumber() {
+ return accountNumber;
+ }
+ public float getBalance() {
+ return balance;
+ }
+ public int getTransaction() {
+ return transaction;
+ }
+ public float getMonthlyEnd() {
+ return balance;
+ }
+ public Customer getCustomer() {
+ return customer;
+ }
+ public String getAccountType() {
+ return accountType;
+ }
 
-    //setters for attributes
-    public void setAccount ( int accountNumber ) {
-    	this.accountNumber = accountNumber;
-    }
-    public void setBalance ( float balance ) {
-    	this.balance = balance;
-    }
-    public void setTransaction ( int transaction ) {
-    	this.transaction = transaction;
-    }
-    public void setMonthlyEnd ( float monthlyEnd ) {
-    	this.monthlyEnd = monthlyEnd;
-    }
-    public void setCustomer ( Customer customer ) {
-    	this.customer = customer;
-    }
-    public void setAccountType ( String accountType) {
-    	this.accountType = accountType;
-    }
-    
-    //getters for attributes
-    public int getAccountNumber() {
-    	return accountNumber;
-    }
-    public float getBalance() {
-    	return balance;
-    }
-    public int getTransaction() {
-    	return transaction;
-    }
-    public float getMonthlyEnd() {
-    	return balance;
-    }
-    public Customer getCustomer() {
-    	return customer;
-    }
-    public String getAccountType() {
-    	return accountType;
-    }
-    
 }
--- Gold.java
+++ Gold.java
@@ -1,27 +1,26 @@
-import java.io.Serializable;
 
+import java.io.Serializable;
 public class Gold extends Account implements Serializable {
 
-	
-	public Gold (Customer customer, int accountNumber, float balance, int transaction, String accountType) {
-		super (customer, accountNumber, balance, transaction, accountType);
+ public Gold (Customer customer, int accountNumber, float balance, int
+transaction, String accountType) {
+ super (customer, accountNumber, balance, transaction, accountType);
 }
+ public void setMonthlyEnd(float monthlyEnd) {
+ this.monthlyEnd = monthlyEnd;
+ }
 
-	public void setMonthlyEnd(float monthlyEnd) {
-		this.monthlyEnd = monthlyEnd;
-	}
-	
-	public float getMonthlyEnd() {
-		return (balance * 1.05f);
-	}
-	
-	//to string method for Account class
-    public String toString() {
-    	String output = this.customer.toString();
-    	output += "Account Number: " + this.accountNumber;
-    	output += "\nBalance: " + this.balance; 
-    	output += "Account Type: " + this.accountType + "\n";
-    	
-    	return output;
-    }
+ public float getMonthlyEnd() {
+ return (balance * 1.05f);
+ }
+
+ //to string method for Account class
+ public String toString() {
+ String output = this.customer.toString();
+ output += "Account Number: " + this.accountNumber;
+ output += "\nBalance: " + this.balance;
+ output += "Account Type: " + this.accountType + "\n";
+
+ return output;
+ }
 }
